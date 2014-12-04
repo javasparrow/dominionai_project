4c86f7c83bb606a0fbfac27e11b97b55a9d948ed
YukiMurakami <muru31415926@gmail.com>
2014-12-05 00:51:09 +0900

    泥棒学習＋プレイ表示　実装完了

--- learn/ActionLearning/sample.cpp
+++ learn/ActionLearning/sample.cpp
@@ -72,6 +72,60 @@ void cellarSample::show() {
     showGain(_answerSelectCards);
 }
 
+thiefSample::thiefSample(int id,string str) {
+    _sampleid = id;
+    _hand.clear();
+    //feature/hand/answerSelectCard
+    vector<string> out = SpritString(str,"/");
+    if(out.size() != 4) {
+        cout << "reading teacherData error: not match format '/' " << endl;
+        cout << out.size() << endl;
+        exit(0);
+    }
+    for(int i=0;i<4;i++) {
+        if(out[i] != "") {
+            vector<string> out2 = SpritString(out[i], ",");
+            if(i==0) {
+                //feature
+                double val=0.0;
+                for(unsigned int j=0;j<out2.size();j++) {
+                    val = atof( out2[j].c_str() );
+                    _feature.push_back(val);
+                    if(val != 0) {
+                        _notZero.push_back(j);
+                    }
+                }
+            }
+            if(i==1) {
+                //hand
+                for(unsigned int j=0;j<out2.size();j++) {
+                    _hand.push_back(atoi(out2[j].c_str()));
+                }
+            }
+            if(i==2) {
+                //answerSelectCard
+                if(out2.size() != 1) {
+                    cout << "error: size of answerSelectCard != 1" << endl;
+                    exit(0);
+                }
+                _answerSelectCard = atoi(out2[0].c_str());
+            }
+            if(i==3) {
+                //isGainFlag
+                if(out2.size() != 1) {
+                    cout << "error: isGainFlag's size != 1" << endl;
+                    exit(0);
+                }
+                if(atoi(out2[0].c_str()) == 0) {
+                    _isDiscard = false;
+                } else {
+                    _isDiscard = true;
+                }
+            }
+        }
+    }
+}
+
 remodelSample::remodelSample(int id,string str) {
     _sampleid = id;
     //feature/hand/answerSelectCard
